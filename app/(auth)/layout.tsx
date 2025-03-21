7184211296c0a8adadc02cda98e7a8c5a370d7d5
Azhari Muhammad M <azhari.muhammad@ruangguru.com>
2025-03-21 15:00:57 +0700

    init and folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      AuthLayout
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
