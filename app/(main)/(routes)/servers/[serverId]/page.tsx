36656ba5fa49d7d54d297fd83e9d8daa95282387
Azhari Muhammad M <azhari.muhammad@ruangguru.com>
2025-03-25 03:16:23 +0700

    add navigation sidebar

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,5 @@
+const ServerIdPage = () => {
+  return <div>ServerPage ID</div>;
+};
+
+export default ServerIdPage;
--- components/modals/InitialModal.tsx
+++ components/modals/InitialModal.tsx
@@ -3,6 +3,7 @@
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 
 import {
   Dialog,
@@ -25,6 +26,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import FileUpload from "@/components/FileUpload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -36,6 +38,7 @@ const formSchema = z.object({
 });
 
 const InitialModal = () => {
+  const router = useRouter();
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -47,7 +50,14 @@ const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log("here:", values);
+    try {
+      await axios.post("/api/servers", values);
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
