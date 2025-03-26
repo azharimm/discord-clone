18649af33805c312e58fef5a5319080a25d1c3de
Azhari Muhammad M <azhari.muhammad@ruangguru.com>
2025-03-27 04:55:57 +0700

    add invite peole functionality

--- components/navigation/NavigationItem.tsx
+++ components/navigation/NavigationItem.tsx
@@ -34,7 +34,7 @@ const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
               "bg-primary/10 text-primary rounded-[16px]"
           )}
         >
-          <Image fill src={imageUrl} alt="Channel" />
+          <Image fill src={imageUrl} alt="Channel" className="object-cover" />
         </div>
       </button>
     </ActionTooltip>
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "@/components/modals/CreateServerModal";
+import { InviteModal } from "@/components/modals/InviteModal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -16,6 +17,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
     </>
   );
 };
