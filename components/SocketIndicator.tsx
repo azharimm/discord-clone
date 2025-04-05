7b5c0aef045430f3818c65ffc824137eddec789f
Azhari Muhammad M <azhari.muhammad@ruangguru.com>
2025-04-05 15:11:52 +0700

    add connected socket indicator

---
+++ components/SocketIndicator.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useSocket } from "@/components/providers/socket-provider";
+import { Badge } from "@/components/ui/badge";
+
+export const SocketIndicator = () => {
+  const { isConnected } = useSocket();
+
+  if (!isConnected) {
+    return (
+      <Badge className="bg-yellow-600 text-white border-none" variant="outline">
+        Fallback: Polling every 1s
+      </Badge>
+    );
+  }
+
+  return (
+    <Badge className="bg-emerald-600 text-white border-none" variant="outline">
+      Connected
+    </Badge>
+  );
+};
